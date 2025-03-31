083f1af898d2286bf135fd1d4b9b02bc7adc4e42
giu <giulia.13.09@hotmail.com>
2025-03-31 17:45:59 +0200

    se creo busqueda

--- app/src/main/java/com/example/zodiacapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/zodiacapp/activities/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.zodiacapp.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,11 +16,13 @@ import com.example.zodiacapp.HoroscopeAdapter
 import com.example.zodiacapp.R
 import com.example.zodiacapp.data.HoroscopeProvider
 
+
 class MainActivity : AppCompatActivity() {
 
     var horoscopeList = HoroscopeProvider.getAll()
 
     lateinit var recyclerView: RecyclerView
+    lateinit var adapter: HoroscopeAdapter
 
 
 
@@ -33,7 +38,7 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView = findViewById(R.id.recyclerView)
 
-        val adapter = HoroscopeAdapter(horoscopeList, { position ->
+        adapter = HoroscopeAdapter(horoscopeList, { position ->
             val horoscope = horoscopeList[position]
 
             val intent = Intent(this, DetailActivity::class.java)
@@ -44,10 +49,38 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_activity_main,menu)
+
+        val menuItem = menu.findItem(R.id.menu_search)
+        val searchView = menuItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : OnQueryTextListener{
+           // CUANDO EL USUARIO LE DE EL BOTON DE BUSCAR
+            override fun onQueryTextSubmit(query: String): Boolean {
+
+                return false
+
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                horoscopeList = HoroscopeProvider.getAll().filter { horoscope ->
+                    //es igual a filtro
+                   getString(horoscope.name).contains(newText,true)
+
+                }
+                adapter.updateItems(horoscopeList)
+                return true
+            }
+
+        })
+
+
+
         return true
     }
+
+
 }
 
 
